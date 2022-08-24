2298a43741f4b590984ae08ecc5ff126277f6edc
Himalaya <himalaya341@gmail.com>
2022-08-24 11:55:38 +0530

    API

---
+++ back-end/Walkabout_API/Walkabout_API/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Walkabout_API.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Carts = new HashSet<Cart>();
+            Orders = new HashSet<Order>();
+            Wishlists = new HashSet<Wishlist>();
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string ImagePath { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductRating { get; set; }
+        public double ProductOfferPrice { get; set; }
+        public string ProductCategory { get; set; }
+
+        public virtual ICollection<Cart> Carts { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<Wishlist> Wishlists { get; set; }
+    }
+}
---
+++ back-end/Walkabout_API/Walkabout_API/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Walkabout_API.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Carts = new HashSet<Cart>();
+            Orders = new HashSet<Order>();
+            Wishlists = new HashSet<Wishlist>();
+        }
+
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string EmailId { get; set; }
+        public string Address { get; set; }
+
+        public virtual ICollection<Cart> Carts { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<Wishlist> Wishlists { get; set; }
+    }
+}
