634da2b27d0692f8e78960f026574fa98067091b
Himalaya <himalaya341@gmail.com>
2022-09-05 20:25:45 +0530

    Changes in header

--- front-end/src/app/header/header.component.ts
+++ front-end/src/app/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { CartComponent } from '../cart/cart.component';
+import { CartServices } from '../cart/cart.services';
+import { WishlistServices } from '../wishlist/wishlist.services';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +11,35 @@ import { CartComponent } from '../cart/cart.component';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cartservice: CartServices,
+              private wishlistservice:WishlistServices) {
+
+   }
   public sessionStorage = sessionStorage;
+  userId: any;
+  cartList:any;
+  cartListLength:number;
+  wishListLength:number;
+  wishList:any;
   usercheck = Boolean(sessionStorage.getItem('UserID'))
-  ngOnInit(): void 
+  ngOnInit(): void
   {
+    this.userId = this.sessionStorage.getItem('UserID');
+    if (this.userId != 0)
+      this.cartservice
+        .getAllCartInfo(this.userId)
+        .subscribe((res) => {
+
+          this.cartList = res;
+          this.cartListLength = this.cartList.length;
+          console.log("sdviubsdiv",this.cartListLength);
+        });
+        this.wishlistservice
+        .getAllWishlistInfo(this.userId)
+        .subscribe((res) => {
+          this.wishList = res;
+          this.wishListLength = this.wishList.length;
+        });
   }
   logout():void
   {
